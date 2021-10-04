56e5b8991b47dd685bb6e581eee18d994e6e1bd3
Victor <victor.j.forouhar@durham.ac.uk>
2021-10-04 17:37:55 +0100

    Added tests To check whether number of subfiles is correct

--- tests/test_read_hdf5.py
+++ tests/test_read_hdf5.py
@@ -1,13 +1,14 @@
+
 import pytest
 from hdf5Lib import hdf5Lib
 
-# @pytest.fixture(
-
-# )
-
+#=======================================================================
+# Testing generation of self.file_list
+#=======================================================================
 def test_single_file_string():
-    hdf5_file = hdf5Lib.read_hdf5('../examples/single_file/file_hdf5',is_split=False)
-    assert hdf5_file.file_list == ['../examples/single_file/file_hdf5']
+    hdf5_file = hdf5Lib.read_hdf5('../examples/single_file/file.hdf5',is_split=False)
+    assert hdf5_file.file_list == ['../examples/single_file/file.hdf5']
+
 
 def test_formatted_file_string():
     file_list = [f"../examples/split_files/subfile_{x:02d}.hdf5" for x in range(50)]
@@ -19,9 +20,26 @@ def test_list_of_files_strings():
     hdf5_file = hdf5Lib.read_hdf5(file_list,is_split=True)
     assert hdf5_file.file_list == file_list
 
-# def test_get_number_subfiles_single_file(): 
-#     assert hdf5_file.number_subfiles == 1
-# def test_get_number_subfiles_formatted_file_string():
-#     assert hdf5_file.number_subfiles == 50
-# def test_get_number_subfiles_list_of_files():
-#     assert hdf5_file.number_subfiles == 50
+#=======================================================================
+# Testing whether correct number of subfiles are being calculated
+#=======================================================================
+
+def test_get_number_subfiles_single_file():
+    hdf5_file = hdf5Lib.read_hdf5('../examples/single_file/file.hdf5',is_split=False)
+    assert hdf5_file.number_subfiles == 1
+
+def test_get_number_subfiles_formatted_file_string():
+    file_list = [f"../examples/split_files/subfile_{x:02d}.hdf5" for x in range(50)]
+    hdf5_file = hdf5Lib.read_hdf5('../examples/split_files/subfile_%.2d.hdf5',is_split=True,number_subfiles=50)
+    assert hdf5_file.number_subfiles == 50
+
+def test_get_number_subfiles_list_of_files_strings():
+    file_list = [f"../examples/split_files/subfile_{x:02d}.hdf5" for x in range(50)]
+    hdf5_file = hdf5Lib.read_hdf5(file_list,is_split=True)
+    assert hdf5_file.number_subfiles == 50
+
+def test_unspecified_number_subfiles():
+    with pytest.raises(ValueError):
+        hdf5Lib.read_hdf5('../examples/split_files/subfile_%.2d.hdf5',is_split=True)
+    
+
