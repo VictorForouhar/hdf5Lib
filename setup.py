17c5ea35fcde1a2fe363d53f405700bcf126c3d8
Victor Forouhar <victor.j.forouhar@durham.ac.uk>
2022-01-12 10:19:42 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='hdf5Lib',
-      version='0.0.1',
+      version='2022.1',
       description='Python3 package used for paralell reading of HDF5 files that have been split into several files.',
       author='Victor Forouhar Moreno',
       author_email='victor.j.forouhar@durham.ac.uk',
